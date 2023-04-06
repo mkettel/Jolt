08eb04315f212c5208086793534eb53965fa6e52
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-04-06 12:46:15 -0500

    got the downloaded video to appeaar on extension. Need to make the grid look better and be all flexed out n shit

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 // import home css page
 import '../css/home.css';
+import ReactPlayer from 'react-player';
+
 
 
 export const Home = () => {
@@ -12,8 +14,11 @@ export const Home = () => {
           <h1 className='mainTitle'>Jolt</h1>
         </div>
         <div className='content'>
-          <div className='grid-content'>
-
+          <div className='gridContent'>
+          <VideoPlayer />
+          <VideoPlayer />
+          <VideoPlayer />
+          <VideoPlayer />
           </div>
         </div>
       </div>
@@ -22,3 +27,21 @@ export const Home = () => {
 
 
 // components
+function VideoPlayer() {
+
+
+
+  return (
+    <div className='video'>
+      <ReactPlayer
+        className='react-video'
+        url="videos/cc_AI.mp4"
+        width='200px'
+        height='200px'
+        controls={true}
+        />
+    </div>
+
+  )
+}
+// https://www.youtube.com/watch?v=dQw4w9WgXcQ
