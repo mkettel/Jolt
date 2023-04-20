797068cde10a9f3d36e5617607e8c58813227a3e
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-04-20 13:04:21 -0400

    tested the youtube url. Will change so no need for shareableURL and just use URLs directly in the db

--- src/components/video.js
+++ src/components/video.js
@@ -146,14 +146,14 @@ const videos = [
     url: "videos/trending/home_science_egg.mp4",
     page: ['trending', 'science'],
     shareURL: 'https://drive.google.com/file/d/17UohK7wAtdauH8p8Fwt-GDj_JnIPxz_D/view?usp=share_link'
-  },
-  {
-    id: 20,
-    title: 'What are Waves?',
-    url: "https://www.youtube.com/watch?v=2Xb6Ao6s8KQ&list=PL2AC33DF4C97B904A&index=8",
-    page: ['science'],
-    shareURL: 'https://www.youtube.com/watch?v=2Xb6Ao6s8KQ&list=PL2AC33DF4C97B904A&index=8'
   }
+  // {
+  //   id: 20,
+  //   title: 'What are Waves?',
+  //   url: "https://www.youtube.com/watch?v=2Xb6Ao6s8KQ&list=PL2AC33DF4C97B904A&index=8",
+  //   page: ['science'],
+  //   shareURL: 'https://www.youtube.com/watch?v=2Xb6Ao6s8KQ&list=PL2AC33DF4C97B904A&index=8'
+  // }
 ];
 
 // ****************************************
@@ -201,28 +201,25 @@ export const VideoList = (props) => {
     setDragItem(video.shareURL); // set the item being dragged to be the video object
     e.dataTransfer.setData('text/plain', dragItem);
     // e.dataTransfer.dropEffect = 'copy'; // set the drop effect to be copy
-
   }
-
-
 
   function handleDragEnd(e, video) {
     e.preventDefault();
     console.log(dragItem)
   }
 
-  // When the item is dropped over the drop zone
-  function handleDragOver(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
-  }
-  // When the item is dropped in the drop zone
-  function dropHandler(e) {
-    e.preventDefault();
-    const data = e.dataTransfer.getData('text/plain');
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + data, '_blank', 'width=600,height=400') // copies to new tab if dragged there.
-    e.target.value = data;
-  }
+  // // When the item is dropped over the drop zone
+  // function handleDragOver(e) {
+  //   e.preventDefault();
+  //   e.dataTransfer.dropEffect = 'copy';
+  // }
+  // // When the item is dropped in the drop zone
+  // function dropHandler(e) {
+  //   e.preventDefault();
+  //   const data = e.dataTransfer.getData('text/plain');
+  //   window.open('https://www.facebook.com/sharer/sharer.php?u=' + data, '_blank', 'width=600,height=400') // copies to new tab if dragged there.
+  //   e.target.value = data;
+  // }
 
 
 
